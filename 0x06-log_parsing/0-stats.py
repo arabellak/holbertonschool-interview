f0860af8b0e61c9e93744abca316f63068e7485c
bxrbxrx <barbaracalle10i3@gmail.com>
2021-01-28 22:38:34 -0500

    reads stdin line by line and computes metrics:

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,9 +6,9 @@ Script that reads stdin line by line and computes metrics
 
 import sys
 
+stat = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+          '404': 0, '405': 0, '500': 0}
 
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
-                '405': 0, '500': 0}
 counter = 0
 size = 0
 
@@ -21,19 +21,19 @@ try:
             counter += 1
             if counter <= 10:
                 size += int(data[0])
-                code = data[1]
+                codes = data[1]
 
-            if code in status_codes.keys():
-                status_codes[code] += 1
+            if codes in stat.keys():
+                stat[codes] += 1
 
             if counter == 10:
                 print('File size: {}'.format(size))
-                for statcode, v in sorted(status_codes.items()):
+                for state_code, v in sorted(stat.items()):
                     if v != 0:
-                        print('{}: {}'.format(statcode, v))
+                        print('{}: {}'.format(state_code, v))
                 counter = 0
 finally:
     print('File size: {}'.format(size))
-    for statcode, v in sorted(status_codes.items()):
+    for state_code, v in sorted(stat.items()):
         if v != 0:
-            print('{}: {}'.format(statcode, v))
+            print('{}: {}'.format(state_code, v))
